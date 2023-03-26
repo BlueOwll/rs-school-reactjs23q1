03d196eb291d5ed8d8e77ee86402426d32e099d2
BlueOwll <blueowl.n@gmail.com>
2023-03-26 18:45:02 +0300

    fix: fix Card test props

--- react-tasks/src/App/App.tsx
+++ react-tasks/src/App/App.tsx
@@ -23,4 +23,3 @@ class App extends React.Component {
 }
 
 export default App;
-
--- react-tasks/src/components/Card/Card.test.tsx
+++ react-tasks/src/components/Card/Card.test.tsx
@@ -1,10 +1,17 @@
 import { render, screen } from '@testing-library/react';
 
-import { Card } from './Card';
+import { Card, ICardProps } from './Card';
+
+const testCard: ICardProps = {
+  imgPath: '/img/norvegskaya-3.jpg',
+  title: 'Norwegian cat',
+  descr: 'description',
+  story: 'story',
+};
 
 describe('<Card />', () => {
   it('renders Card component', () => {
-    render(<Card />);
+    render(<Card {...testCard} />);
     screen.debug();
   });
 });
