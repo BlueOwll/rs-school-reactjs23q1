ca49feee20eb5e4b87d55d8af23fb94a8fe03df8
BlueOwll <blueowl.n@gmail.com>
2023-04-02 00:51:59 +0300

    feat: convert Card & Cards class components to function

--- react-tasks/src/components/Card/Card.tsx
+++ react-tasks/src/components/Card/Card.tsx
@@ -9,31 +9,21 @@ export interface ICardProps {
   breed: string;
   fromShelter: boolean;
 }
-export class Card extends React.Component<ICardProps> {
-  constructor(props: ICardProps) {
-    super(props);
-    // window.URL.createObjectURL()
-  }
 
-  render() {
-    const src =
-      typeof this.props.imgPath === 'string'
-        ? this.props.imgPath
-        : window.URL.createObjectURL(this.props.imgPath);
+export const Card = (props: ICardProps) => {
+  const src =
+    typeof props.imgPath === 'string' ? props.imgPath : window.URL.createObjectURL(props.imgPath);
 
-    return (
-      <div className="card">
-        <img src={src} alt="" className="card__img" />
-        <div className="card__wrapper">
-          <h4 className="card__title">{this.props.name}</h4>
-          <p className="card__story">Breed: {this.props.breed}</p>
-          <p className="card__story">Sex: {this.props.sex}</p>
-          <p className="card__story">
-            {this.props.birthday ? `Birthday: ${this.props.birthday}` : ''}
-          </p>
-          <p className="card__story">{this.props.fromShelter ? 'from shelter' : ''}</p>
-        </div>
+  return (
+    <div className="card">
+      <img src={src} alt="" className="card__img" />
+      <div className="card__wrapper">
+        <h4 className="card__title">{props.name}</h4>
+        <p className="card__story">Breed: {props.breed}</p>
+        <p className="card__story">Sex: {props.sex}</p>
+        <p className="card__story">{props.birthday ? `Birthday: ${props.birthday}` : ''}</p>
+        <p className="card__story">{props.fromShelter ? 'from shelter' : ''}</p>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
